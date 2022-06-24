08df93ab43839cbefaa5570953f65c2776f34ccb
Davide Giardini <98034877+DavideGiardini@users.noreply.github.com>
2022-06-24 19:26:21 +0200

    Create modifiers.R

---
+++ Sentiment/modifiers.R
@@ -0,0 +1,5 @@
+polarityShifter <- c("isn't", "aren't", "wasn't", "weren't", "hasn't", "haven't", "hadn't", "doesn't", "don't", "didn't", "won't", "wouldn't", "couldn't", "mustn't", "against", "no", "nor", "not")
+
+intensifier <- c("further", "all", "more", "most", "too", "very", "much", "many", "far", "truly", "really", "extremely", "highly", "specially", "particulary", "especially", "mostly", "great", "large", "big", "loud", "strong", "high", "exceptional", "awesome", "outstanding", "incredible", "incredibly", "unbelieavable", "remarkable", "remarkably", "considerable", "considerably")
+
+weakener <- c("few", "only", "just", "minimal", "minimally", "little", "short", "shortly", "fewer", "partial", "partially", "least", "almost", "nearly", "about")
